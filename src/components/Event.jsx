b365ad9f126472a50209e2ad7db86b9a7ab18897
Yassine Mili <ya.mili@icloud.com>
2024-11-25 12:25:53 +0100

    date edited

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -57,25 +57,32 @@ const Event = () => {
   const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    const target = new Date("2024-11-13T10:00:00");
+    const target = new Date("2024-11-12T10:00:00");
 
     const interval = setInterval(() => {
       const now = new Date();
       const difference = target.getTime() - now.getTime();
 
       const d = Math.floor(difference / (1000 * 60 * 60 * 24));
-      setDays(d);
 
       const h = Math.floor(
         (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
       );
-      setHours(h);
 
       const m = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-      setMinutes(m);
 
       const s = Math.floor((difference % (1000 * 60)) / 1000);
-      setSeconds(s);
+      if (difference <= 0) {
+        setDays(0);
+        setHours(0);
+        setMinutes(0);
+        setSeconds(0);
+      } else {
+        setHours(h);
+        setMinutes(m);
+        setSeconds(s);
+        setDays(d);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
